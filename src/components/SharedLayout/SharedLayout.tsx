b5d4765c6964daf1ba0a74646c91564d396caf84
Phuc Le <phuclevinh2000@gmail.com>
2022-10-25 22:59:21 +0300

    Fixing deployment error

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -1,7 +1,6 @@
 import './SharedLayout.scss';
 import { Outlet } from 'react-router-dom';
 import BigSidebar from '../BigSidebar/BigSidebar';
-import SmallSidebar from '../SmallSidebar/SmallSidebar';
 
 const SharedLayout = () => {
   return (
